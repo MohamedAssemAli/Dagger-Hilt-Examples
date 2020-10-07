1a4624421533ef94c9b807716316b6782bdcc09b
Mohamed Assem <mohamed.assem@orchtech.com>
2020-10-07 15:23:01 +0200

    Initial commit

---
+++ app/src/main/java/com/assem/daggerhiltexamples/MyApp.kt
@@ -0,0 +1,16 @@
+package com.assem.daggerhiltexamples
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Mohamed Assem on 10/7/2020.
+ * mohamed.assem.ali@gmail.com
+ * https://github.com/MohamedAssemAli
+ * Company: Orchtech
+ */
+@HiltAndroidApp
+class MyApp : Application() {
+    
+}
