966cd92fae6d002dea7ba4a46499f39472921695
Mohamed Assem <mohamed.assem@orchtech.com>
2020-12-22 17:54:13 +0200

    Initial network layer setup

--- app/src/main/java/com/assem/daggerhiltexamples/mindorks_example/di/ApplicationModule.kt
+++ app/src/main/java/com/assem/daggerhiltexamples/mindorks_example/di/ApplicationModule.kt
@@ -1,9 +1,54 @@
 package com.assem.daggerhiltexamples.mindorks_example.di
 
+import com.assem.daggerhiltexamples.BuildConfig
+import com.assem.daggerhiltexamples.mindorks_example.data.api.ApiHelper
+import com.assem.daggerhiltexamples.mindorks_example.data.api.ApiHelperImpl
+import com.assem.daggerhiltexamples.mindorks_example.data.api.ApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
-class ApplicationModule {}
+class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = BuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else OkHttpClient
+        .Builder()
+        .build()
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiHelper(apiHelper: ApiHelperImpl): ApiHelper = apiHelper
+
+}
